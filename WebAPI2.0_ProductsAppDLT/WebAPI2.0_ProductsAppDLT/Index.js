43cd96e2d7acdb18cc982f7a3efbb26e874a3f77
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2018-09-27 10:03:13 -0400

    AddedJQuery_HTTPRequest

---
+++ WebAPI2.0_ProductsAppDLT/WebAPI2.0_ProductsAppDLT/Index.js
@@ -0,0 +1,27 @@
+﻿"https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.min.js";
+$(document).ready(function () {
+	// Send an AJAX request
+	$.getJSON(uri)
+		.done(function (data) {
+			// On success, 'data' contains a list of products.
+			$.each(data, function (key, item) {
+				// Add a list item for the product.
+				$('<li>', { text: formatItem(item) }).appendTo($("#products"));
+			});
+		});
+});
+
+function formatItem(item) {
+	return item.Name + ': $' + item.Price;
+}
+
+function find() {
+	var id = $("#prodId").val();
+	$.getJSON(uri + '/' + id)
+		.done(function (data) {
+			$("#product").text(formatItem(data));
+		})
+		.fail(function (jqXHR, textStatus, err) {
+			$("#product").text('Error: ' + err);
+		});
+}
---
+++ WebAPI2.0_ProductsAppDLT/WebAPI2.0_ProductsAppDLT/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI2._0_ProductsAppDLT.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
